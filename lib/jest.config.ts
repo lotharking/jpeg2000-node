2e21681f0dec8c7ae03ef53d4466d8c803e2389c
Andres Vallecilla <andresfelipe083195@hotmail.com>
2025-03-21 23:01:03 -0500

    fix: validations

--- lib/jest.config.ts
+++ lib/jest.config.ts
@@ -3,6 +3,11 @@ import type { Config } from '@jest/types'
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json'
+    }
+  },
   coveragePathIgnorePatterns: ['/build/', '/node_modules/', '/__tests__/', 'tests'],
   coverageDirectory: '<rootDir>/coverage/',
   testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
